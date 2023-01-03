2c4f9e641007e25e1703b062bd2f421feeafbdf0
Sanctfi <jhn.hines@gmail.com>
2023-01-03 17:03:24 -0600

    REST API - Get and Post

--- 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
 @Service
@@ -13,8 +14,15 @@ public class ContactServiceImpl implements ContactService {
     @Autowired
     private ContactRepository contactRepository;
     
+    @Override
+    public Contact getContactById(String id) {
+        return contactRepository.getContact(findIndexById(id));
+    }
 
-
+    @Override
+    public void saveContact(Contact contact) {
+        contactRepository.saveContact(contact);
+    }
 
     private int findIndexById(String id) {
         return IntStream.range(0, contactRepository.getContacts().size())
@@ -22,5 +30,6 @@ public class ContactServiceImpl implements ContactService {
             .findFirst()
             .orElseThrow();
     }
+
 
 }
