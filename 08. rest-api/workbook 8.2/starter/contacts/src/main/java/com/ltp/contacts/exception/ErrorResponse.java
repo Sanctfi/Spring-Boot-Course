b000a014809b9dd8c9f66891f20db8467eb93222
Sanctfi <jhn.hines@gmail.com>
2023-01-05 19:28:31 -0600

    Worked through up to Challenge for Testing

--- 08. rest-api/workbook 8.2/starter/contacts/src/main/java/com/ltp/contacts/exception/ErrorResponse.java
+++ 08. rest-api/workbook 8.2/starter/contacts/src/main/java/com/ltp/contacts/exception/ErrorResponse.java
@@ -1,7 +1,6 @@
 package com.ltp.contacts.exception;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -9,11 +8,12 @@ public class ErrorResponse {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
-    private List<String> message;
+    
+    private String message;
 
-    public ErrorResponse(List<String> message) {
-        this.timestamp = LocalDateTime.now();
+    public ErrorResponse(String message) {
         this.message = message;
+        this.timestamp = LocalDateTime.now();
     }
 
     public LocalDateTime getTimestamp() {
@@ -24,11 +24,11 @@ public class ErrorResponse {
         this.timestamp = timestamp;
     }
 
-    public List<String> getMessage() {
+    public String getMessage() {
         return this.message;
     }
 
-    public void setMessage(List<String> message) {
+    public void setMessage(String message) {
         this.message = message;
     }
     
